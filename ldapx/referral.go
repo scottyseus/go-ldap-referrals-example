bd3613da8a70ab6ff63f190d38d3788ca28551be
scottyseus <sweidenkopf@gmail.com>
2020-05-23 10:40:33 -0400

    initiall full functionality

--- ldapx/referral.go
+++ ldapx/referral.go
@@ -1,4 +1,4 @@
-package main
+package ldapx
 
 import (
 	"context"
@@ -7,6 +7,17 @@ import (
 	"net"
 	"net/url"
 )
+
+type CompositeError struct {
+	Errs []error
+}
+
+func (err CompositeError) Error() string {
+	if err.Errs == nil || len(err.Errs) < 1 {
+		return "no errors found"
+	}
+	return "multiple errors found"
+}
 
 type DeepSearchRequest struct {
 	*ldap.SearchRequest
@@ -28,6 +39,8 @@ func DeepSearch(dialer Dialer, ldapURL *url.URL, req DeepSearchRequest) (*ldap.S
 	entries := make([]*ldap.Entry, 0, 100)
 	result := new(ldap.SearchResult)
 
+	errs := make([]error, 0)
+
 	for {
 		if len(refs) < 1 {
 			break
@@ -37,10 +50,12 @@ func DeepSearch(dialer Dialer, ldapURL *url.URL, req DeepSearchRequest) (*ldap.S
 		refs = refs[1:]
 		refURL, err := url.Parse(ref)
 		if err != nil {
+			errs = append(errs, err)
 			continue
 		}
-		result, err := requestAll(dialer, refURL, req)
+		result, err := submitRequest(dialer, refURL, req)
 		if err != nil {
+			errs = append(errs, err)
 			continue
 		}
 		if result.Entries != nil {
@@ -49,25 +64,23 @@ func DeepSearch(dialer Dialer, ldapURL *url.URL, req DeepSearchRequest) (*ldap.S
 		refs = append(refs, result.Referrals...)
 	}
 
+	if len(errs) > 0 {
+		return result, CompositeError{Errs:errs}
+	}
 	return result, nil
 }
 
-func requestAll(dialer Dialer, ldapURL *url.URL, req DeepSearchRequest) (*ldap.SearchResult, error) {
+func submitRequest(dialer Dialer, ldapURL *url.URL, req DeepSearchRequest) (*ldap.SearchResult, error) {
 	netConn, err:= dialer.Dial("tcp", fmt.Sprintf("%s:%s", ldapURL.Hostname(), ldapURL.Port()))
 	if err != nil {
 		return nil, err
 	}
 	conn := ldap.NewConn(netConn, ldapURL.Scheme == "ldaps")
-
 	if req.Username != "" {
 		err = conn.Bind(req.Username, req.Password)
-		bindReq := ldap.NewSimpleBindRequest(req.Username, req.Password, req.Controls)
-		bindReq.AllowEmptyPassword = req.AllowAnonymousBind
-		conn.SimpleBind(bindReq)
 		if err != nil {
 			return nil, err
 		}
 	}
-
 	return conn.Search(req.SearchRequest)
 }
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-ldap/ldap/v3"
+	"ldap-referral/ldapx"
 	"net"
 	"net/url"
+	"time"
 )
 
 func main() {
@@ -20,16 +23,21 @@ func main() {
 		[]string{"uid", "dn"},
 		nil)
 
-	results, err := DeepSearch(new(net.Dialer), ldapURL, DeepSearchRequest{
+	dialer := new(net.Dialer)
+
+	dialer.Timeout = time.Second * 4
+
+	results, err := ldapx.DeepSearch(dialer, ldapURL, ldapx.DeepSearchRequest{
 		SearchRequest: req,
 		Username:      "cn=admin,dc=test,dc=com",
 		Password:      "admin",
 	})
 
-	if err != nil {
-		panic(err)
-	} else {
-		results.Print()
-	}
+	fmt.Printf("result: %+v\nerrors: %+v\n", results, err)
 
+	if compErr, ok := err.(ldapx.CompositeError); ok {
+		for _, e := range compErr.Errs {
+			fmt.Println("\t" + e.Error())
+		}
+	}
 }
